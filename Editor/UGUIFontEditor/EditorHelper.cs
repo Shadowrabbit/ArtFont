b07534460dc6f5795d3a144ea3dc2923c8ea5979
BILIBILI\wangyue07 <wangyue07@bilibili.com>
2021-04-23 19:31:53 +0800

    1

---
+++ Editor/UGUIFontEditor/EditorHelper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorHelper : MonoBehaviour {
+
+	[MenuItem("Assets/生成艺术字")]
+	public static void BatchCreateArtistFont()
+	{
+		ArtistFont.BatchCreateArtistFont();
+	}
+}
